8bb8118efe8995bd329342e289da95299fd592a4
BibireConstantinMihnea <bibiremihnea@gmail.com>
2023-05-11 13:34:35 +0300

    Added Compulsory

---
+++ Lab10/Compulsory/Server/GameServer.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.serverapplication;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Mihnea
+ */
+public class GameServer {
+
+    public static final int PORT = 8100;
+
+    public void SimpleServer() throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            while (true) {
+                System.out.println("Waiting for a client ...");
+                Socket socket = serverSocket.accept();
+                // Execute the client's request in a new thread
+                new ClientThread(socket).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Ooops... " + e);
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
